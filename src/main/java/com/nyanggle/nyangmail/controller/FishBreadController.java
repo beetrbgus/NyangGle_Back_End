eb832894dca8385d241e834163bb7dfca0fda9f0
beetrbgus <devbeetrb0214@gmail.com>
2022-11-27 22:13:19 +0900

    feature - 붕어빵 하나 가져오기 API 수정
    - 조회시 읽음 추가

--- src/main/java/com/nyanggle/nyangmail/controller/FishBreadController.java
+++ src/main/java/com/nyanggle/nyangmail/controller/FishBreadController.java
@@ -24,24 +24,20 @@ public class FishBreadController {
     /**
      * 한개 가져오기
      * Todo 사용자 토큰을 이용한 검증 필요.
-     * @param uUid
      * @param fishId
      * @return
      */
-    @GetMapping("/{uuid}/{fishUid}")
-    public ResponseEntity findFishBreadByUUID(@PathVariable(name = "uuid") String uUid,
-                                               @PathVariable(name = "fishUid") Long fishId) {
-        return ResponseEntity.ok(fishBreadService.findByFishUid(uUid, fishId));
+    @GetMapping("/{fishId}")
+    public ResponseEntity findFishBreadByUUID(@PathVariable(name = "fishId") Long fishId) {
+        String uUid = "U18414f5037a0001"; // @AuthenticationPrincipal 추가한 후 uUid 받아와서 넣어주기.
+        FishBreadResDto resDto = fishBreadService.findByFishUid(uUid, fishId);
+        fishBreadService.fishBreadstatusChange(fishId);
+        return ResponseEntity.ok(resDto);
     }
 
     @PostMapping("/{uuid}")
     public ResponseEntity createFishBread(@RequestBody @Valid FishBreadCreateReqDto reqDto, @PathVariable(value = "uuid") String receiverUid) {
         fishBreadService.create(reqDto,receiverUid);
         return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    @GetMapping("/{uuid}")
-    public FishBreadResDto findFishBreadAll(@PathVariable(name = "uuid") String uuid) {
-        return null;
     }
 }
