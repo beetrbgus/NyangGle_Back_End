139ff9017b5f3d4079cb4fcc074dc6050cdc0d0c
beetrbgus <devbeetrb0214@gmail.com>
2022-11-28 04:57:01 +0900

    modified - 붕어빵 목록 API
    - 기본 page 1부터 설정
    - 최대 한페이지 크기 45 설정

--- src/main/java/com/nyanggle/nyangmail/config/WebConfig.java
+++ src/main/java/com/nyanggle/nyangmail/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.nyanggle.nyangmail.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfigure implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
     /**
      * Todo 프론트 배포시 allowedOrigin 추가
      * Todo 토큰 적용시 expose Header 추가
@@ -18,5 +20,13 @@ public class CorsConfigure implements WebMvcConfigurer {
                 .allowedOriginPatterns("*")
                 .maxAge(3600);
         WebMvcConfigurer.super.addCorsMappings(registry);
+    }
+
+    @Bean
+    public PageableHandlerMethodArgumentResolverCustomizer customizer() {
+        return pageableResolver -> {
+            pageableResolver.setOneIndexedParameters(true);
+            pageableResolver.setMaxPageSize(45);
+        };
     }
 }
