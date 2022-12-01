137f070b8ba96f363662f2444f6bfd0dee653dd4
beetrbgus <devbeetrb0214@gmail.com>
2022-12-02 03:34:40 +0900

    style - 웹 설정 관련 수정
    - 프론트 url 하드 코딩 -> properties 파일
    
    - allowedHeader 추가
    -- preflight시 오는 헤더들 추가 & 토큰 헤더 추가

--- src/main/java/com/nyanggle/nyangmail/config/WebConfig.java
+++ src/main/java/com/nyanggle/nyangmail/config/WebConfig.java
@@ -1,10 +1,11 @@
 package com.nyanggle.nyangmail.config;
 
 import com.nyanggle.nyangmail.oauth.CustomAuthArgumentResolver;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,19 +13,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
-    /**
-     * Todo 프론트 배포시 allowedOrigin 추가
-     * Todo 토큰 적용시 expose Header 추가
-     * @param registry
-     */
+
+    @Value("${nyangletter.frontEnd.cloudFlare}")
+    private String CLOUDFLARE_PAGES;
+
+    @Value("${nyangletter.frontEnd.aws}")
+    private String AWS;
+
+    @Value("${nyangletter.frontEnd.local}")
+    private String LOCALHOST;
+
+    @Value("${nyangletter.auth.header.scheme}")
+    private String TOKEN_SCHEME;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://nyangnyang-letter.pages.dev")
+                .allowedOrigins(LOCALHOST, AWS, CLOUDFLARE_PAGES)
                 .allowedMethods("*")
                 .allowCredentials(true)
-                .exposedHeaders("X-NYANG-AUTH-TOKEN")
+                .allowedHeaders("Accept", "Content-Type",
+                        "Referer","User-Agent",
+                        TOKEN_SCHEME)
                 .maxAge(3600);
     }
 
